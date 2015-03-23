1e751d081a804d32bd71327c4adca5c89d97dda8
Isaac Hanson <isaac@starlig.ht>
2015-03-23 16:46:39 -0400

    split package installation into two steps, checkout dotfiles simultaneously

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 CACHE=/var/cache/cygsetup
+CACHE_WIN="`cygpath -w $CACHE`"
+CYGEXE=setup-x86_64.exe
+EXEURL=http://cygwin.com/$CYGEXE
+MIRROR=http://mirrors.kernel.org/sourceware/cygwin
+PORTS_MIRROR=http://mirrors.kernel.org/sources.redhat.com/cygwinports
+PORTS_GPG=http://cygwinports.org/ports.gpg
 
 if [ ! -f "$CACHE/.required" ]; then
-    CACHE_WIN="`cygpath -w $CACHE`"
-    CYGEXE=setup-x86_64.exe
-    EXEURL=http://cygwin.com/$CYGEXE
-    MIRROR=http://mirrors.kernel.org/sourceware/cygwin
-    PORTS_MIRROR=http://mirrors.kernel.org/sources.redhat.com/cygwinports
-    PORTS_GPG=http://cygwinports.org/ports.gpg
-    BASE_PKGS=alacarte,chere,git,gtk2.0-engines-murrine,libnotify,psmisc,screen,tree,vim,xfce4-notifyd,xfce4-session
-
+    BASE_PKGS=chere,git
     mkdir -p $CACHE
     cd $CACHE
     wget -q -nv -N $EXEURL
@@ -26,6 +25,9 @@ else
         exit 1
     fi
     cd $HOME
+    BASE_PKGS=alacarte,gtk2.0-engines-murrine,libnotify,psmisc,screen,tree,vim,xfce4-notifyd,xfce4-session
+    echo "Installing remaining packages while grabbing dotfiles"
+    cygstart -- $CACHE/$CYGEXE -g -n -K $PORTS_GPG -s $PORTS_MIRROR -s $MIRROR -q -l "$CACHE_WIN" -P $BASE_PKGS
     git clone -b cygwin https://github.com/starlight/dotfiles.git .dotfiles
 
     WINHOME="`cygpath $USERPROFILE`"
