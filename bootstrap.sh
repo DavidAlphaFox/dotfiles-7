16ecc8046ee7e69a78fabb4fcd424964c206788a
Isaac Hanson <isaac@starlig.ht>
2015-03-23 13:06:54 -0400

    fixes to bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@
 CACHE=/var/cache/cygsetup
 
 if [ ! -f "$CACHE/.required" ]; then
-    echo "installing required packages, run again when complete"
     CACHE_WIN="`cygpath -w $CACHE`"
     CYGEXE=setup-x86_64.exe
     EXEURL=http://cygwin.com/$CYGEXE
@@ -17,14 +16,52 @@ if [ ! -f "$CACHE/.required" ]; then
     wget -nv -N $EXEURL
     chmod +x ./$CYGEXE
 
+    echo "installing required packages, run again when complete"
     cygstart -- $CACHE/$CYGEXE -g -n -K $PORTS_GPG -s $PORTS_MIRROR -s $MIRROR -q -l "$CACHE_WIN" -P $BASE_PKGS
     echo $BASE_PKGS > $CACHE/.required
     exit 1
 else
     cd $HOME
-    git clone -b cygwin https://github.com/starlight/dotfiles.git .dotfiles
-    bash .dotfiles/install.sh
+    echo "cloning dotfiles repository"
+    git clone -q -b cygwin https://github.com/starlight/dotfiles.git .dotfiles
+
+    WINHOME="`cygpath $USERPROFILE`"
+    dir=.dotfiles                    # dotfiles directory
+    olddir=.backup_dotfiles          # old dotfiles backup directory
+    
+    echo "backing-up existing dotfiles, setting up symlinks"
+    mkdir -p $olddir
+    # symlink dotfiles, making backup of existing
+    for file in `ls $dir | grep -v -e 'README.md' -e 'install.sh' -e 'bootstrap.sh'`; do
+        echo $file
+        if [ -e "$file" ]; then 
+            cp -rL .$file $olddir/
+            rm -r .$file
+        fi
+        ln -s $dir/$file .$file
+    done
+    mv .profile $olddir/
+
+    # home directory symlinks
+    ln -s $WINHOME .user
+    ln -s $HOME /...
+
+    # symlinks into windows User directory
+    for i in Desktop Public Documents Downloads Pictures Music Videos Diagrams Templates; do 
+        mkdir -p .user/$i
+        ln -s .user/$i $i
+    done
+
+    echo "installing 'Shell...' context menu"
+    # install shell context-menu
+    chere -u
+    chere -icm -t mintty -s bash -e 'Shell...'
+
+    echo "placing XWin shortcut into Startup"
+    # install XWin to Windows Startup
+    cp $dir/extra/cygwinx.lnk `cygpath $APPDATA`/Microsoft/Windows/Start\ Menu/Programs/Startup/
+
+    echo "Setting focus-follows-mouse, no auto-raise in Windows"
+    .bin/sudo reg import ~/.extra/xmouse.reg
 fi
-
-
 
