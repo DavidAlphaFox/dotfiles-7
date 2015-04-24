8c8b53a385301c0e7caa961ac61a044220c006c0
Isaac W Hanson <isaac@starlig.ht>
2015-04-24 14:37:56 -0400

    using apt-cyg for early bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,58 +6,57 @@ MIRROR=http://mirrors.kernel.org/sourceware/cygwin
 PORTS_MIRROR=http://mirrors.kernel.org/sources.redhat.com/cygwinports
 PORTS_GPG=http://cygwinports.org/ports.gpg
 DOTFILES_GIT=https://github.com/starlight/dotfiles.git
-BASE_PKGS="alacarte,ctags,cygcheck-dep,cygutils-extra,cygutils-x11,dos2unix,file-roller,git-completion,gtk2.0-engines-murrine,gvim,libcryptui-common,libnotify,psmisc,screen,seahorse,seahorse-daemon,seahorse-tool,tree,vim,xfce4-notifyd,xfce4-session"
+BASE_PKGS="alacarte,ctags,cygutils-x11,dos2unix,file-roller,git-completion,gtk2.0-engines-murrine,gvim,libcryptui-common,libnotify,psmisc,screen,seahorse,seahorse-daemon,seahorse-tool,tree,vim,xfce4-notifyd,xfce4-session"
 
-if [ ! -f "/.required" ]; then
-    cd /bin
-    wget -q -nv -N $EXEURL
-    chmod +x ./$CYGEXE
-    echo
-    echo -e "\033[31mInstalling required packages, run again when complete"
-    cygstart -- $CYGEXE -o -g -n -K $PORTS_GPG -s $MIRROR -s $PORTS_MIRROR -q -P $BASE_PKGS
-    touch /.required
-    exit 0
-else
-    if [ -f "/etc/setup/.cygwin-xfce" ]; then
-        echo -e "\033[31mAlready installed"
-        exit 1
-    fi
-    cd $HOME
-    umask 022
-    git clone -b cygwin $DOTFILES_GIT .dotfiles
-    cd .dotfiles; git checkout -f HEAD
-    cd ..
+cd /bin
 
-    WINHOME="`cygpath $USERPROFILE`"
-    dir=.dotfiles                    # dotfiles directory
-    olddir=.backup_dotfiles          # old dotfiles backup directory
-    
-    echo "Backing-up existing dotfiles, setting up symlinks"
-    mkdir -p $olddir
-    for file in `ls $dir | grep -v -e 'README.md' -e 'bootstrap'`; do
-        cp -rL .$file $olddir/ 2>/dev/null
-        rm -rf .$file
-        ln -s $dir/$file .$file
-    done
-    mv .profile $olddir/
+lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
+apt-cyg mirror $MIRROR
+apt-cyg install cygcheck-dep cygutils-extra git wget
+rm apt-cyg
 
-    # home directory symlinks
-    ln -s $WINHOME .user
-    ln -s $HOME /.root
+cd $HOME
+umask 022
+git clone -b cygwin $DOTFILES_GIT .dotfiles
+cd .dotfiles; git checkout -f HEAD
+cd ..
 
-    # symlinks into windows User directory
-    for i in Desktop Public Documents Downloads Pictures Music Videos; do 
-        mkdir -p .user/$i
-        ln -s .user/$i $i
-    done
+WINHOME="`cygpath $USERPROFILE`"
+dir=.dotfiles                    # dotfiles directory
+olddir=.backup_dotfiles          # old dotfiles backup directory
 
-    echo "Placing XWin shortcut into Startup"
-    mkshortcut -w "$HOME" -i/bin/run.exe -j2 -s min -n cygwin-xfce -d startxwin-rootless -a '-w hide /bin/sh.exe ~/.bin/startxwin-rootless' /bin/mintty.exe
+echo "Backing-up existing dotfiles, setting up symlinks"
+mkdir -p $olddir
+for file in `ls $dir | grep -v -e 'README.md' -e 'bootstrap'`; do
+    cp -rL .$file $olddir/ 2>/dev/null
+    rm -rf .$file
+    ln -s $dir/$file .$file
+done
+mv .profile $olddir/
 
-    mv cygwin-xfce.lnk `cygpath $APPDATA`/Microsoft/Windows/Start\ Menu/Programs/Startup/
+# home directory symlinks
+ln -s $WINHOME .user
+ln -s $HOME /.root
 
-    ~/.bin/cygleaf > /etc/setup/.cygwin-xfce
+# symlinks into windows User directory
+for i in Desktop Public Documents Downloads Pictures Music Videos; do 
+    mkdir -p .user/$i
+    ln -s .user/$i $i
+done
 
-    echo
-    echo -e "\033[32Re-login to start cygwin-xfce"
-fi
+echo "Placing XWin shortcut into Startup"
+mkshortcut -w "$HOME" -i/bin/run.exe -j2 -s min -n cygwin-xfce -d startxwin-rootless -a '-w hide /bin/sh.exe ~/.bin/startxwin-rootless' /bin/mintty.exe
+
+mv cygwin-xfce.lnk `cygpath $APPDATA`/Microsoft/Windows/Start\ Menu/Programs/Startup/
+
+wget -q -nv -N $EXEURL
+chmod +x ./$CYGEXE
+echo
+echo -e "\033[31mInstalling required packages, run again when complete"
+cygstart -- $CYGEXE -o -g -n -K $PORTS_GPG -s $MIRROR -s $PORTS_MIRROR -q -P $BASE_PKGS
+
+~/.bin/cygleaf > /etc/setup/.cygwin-xfce
+
+echo
+echo -e "\033[32Re-login to start cygwin-xfce"
+
