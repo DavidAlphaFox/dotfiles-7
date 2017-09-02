0d5be74938da761f08ca3835526addba81202ee8
Isaac W Hanson <isaac@starlig.ht>
2017-09-02 00:25:32 -0400

    jack...

--- base/.local/bin/startjack.sh
+++ base/.local/bin/startjack.sh
@@ -2,11 +2,11 @@
 
 jack_control start
 jack_control ds alsa
-jack_control dps device hw:HD2
+jack_control dps device hw:PCH
 jack_control dps rate 48000
 jack_control dps nperiods 2
 jack_control dps period 64
 sleep 10
 a2jmidid -e &
-sleep 10
-qjackctl &
+#sleep 10
+#cadence
