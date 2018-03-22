a4990dc13742c35a28daa9e41e7d183805a19263
Isaac W Hanson <isaac@starlig.ht>
2018-03-22 18:09:07 -0400

    next on right click "playing"

--- misc/.local/bin/playing.sh
+++ misc/.local/bin/playing.sh
@@ -4,3 +4,8 @@ artist="$(playerctl metadata xesam:artist)"
 if [ -n "$artist" ]; then
   echo "  $artist \"$(playerctl metadata xesam:title)\""
 fi
+
+case $BLOCK_BUTTON in
+  1) playerctl play-pause ;;
+  3) playerctl next ;;
+esac
