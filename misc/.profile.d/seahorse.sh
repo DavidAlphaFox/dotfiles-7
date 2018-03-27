a970c131284e94d645a0cc144e5350b951d69f84
Isaac W Hanson <isaac@starlig.ht>
2018-03-27 11:11:50 -0400

    seahorse fix

--- misc/.profile.d/seahorse.sh
+++ misc/.profile.d/seahorse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SSH_ASKPASS="/usr/lib64/seahorse/seahorse-ssh-askpass"
+export SSH_ASKPASS="/usr/lib/seahorse/ssh-askpass"
 #if hash gnome-keyring-daemon 2>/dev/null; then
 #  export SSH_AUTH_SOCK="/run/user/1000/keyring/ssh"
 #fi
