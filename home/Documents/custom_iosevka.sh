d23365fe82cd212496e3dc54403d6fc7494c5b80
Isaac W Hanson <isaac@starlig.ht>
2018-04-12 14:59:59 -0400

    iosevka +bold

--- home/Documents/custom_iosevka.sh
+++ home/Documents/custom_iosevka.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
-
+yaourt -S --needed --noconfirm otfcc ttfautohint
+npm install
 for DESIGN in term termlig type; do
   CUSTOM_CONFIG="$DESIGN sans ligset-ml v-l-zshaped cv05 v-a-singlestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-brace-straight v-dollar-open"
-  make custom-config set=$DESIGN design=$CUSTOM_CONFIG weights='medium'
+  make custom-config set=$DESIGN design=$CUSTOM_CONFIG weights='medium bold'
   make custom set=$DESIGN
 done
-
+for i in `find dist -iname '*.ttf'`; do
+  cp $i ~/.dotfiles/misc/.fonts/;
+done
