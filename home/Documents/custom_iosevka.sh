f087e4437a7ffba8f3f1deaa9aa13eda66e4ae8a
Isaac W Hanson <ihanson@sevone.com>
2018-05-07 14:19:51 -0400

    various

--- home/Documents/custom_iosevka.sh
+++ home/Documents/custom_iosevka.sh
@@ -2,7 +2,7 @@
 yaourt -S --needed --noconfirm otfcc ttfautohint
 npm install
 for DESIGN in term termlig type; do
-  CUSTOM_CONFIG="$DESIGN sans ligset-ml v-l-zshaped v-i-zshaped v-f-tailed v-a-singlestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-brace-straight v-dollar-open"
+  CUSTOM_CONFIG="$DESIGN sans ligset-ml v-l-zshaped v-i-zshaped v-a-singlestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-brace-straight v-dollar-open"
   make custom-config set=$DESIGN design=$CUSTOM_CONFIG weights='medium bold'
   make custom set=$DESIGN
 done
