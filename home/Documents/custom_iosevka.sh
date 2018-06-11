0d89af4f00ae15829f3f56d5c65491cd939fe12a
Isaac W Hanson <isaac@starlig.ht>
2018-06-11 17:25:30 -0400

    iosevka update

--- home/Documents/custom_iosevka.sh
+++ home/Documents/custom_iosevka.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-yaourt -S --needed --noconfirm otfcc ttfautohint
+yaourt -S --needed --noconfirm otfcc ttfautohint npm
 npm install
 for DESIGN in term termlig type; do
   CUSTOM_CONFIG="$DESIGN sans ligset-ml v-l-zshaped v-i-zshaped v-a-singlestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-brace-straight v-dollar-open"
@@ -7,5 +7,5 @@ for DESIGN in term termlig type; do
   make custom set=$DESIGN
 done
 for i in `find dist -iname '*.ttf'`; do
-  cp $i ~/.dotfiles/misc/.fonts/;
+  cp $i ~/.dotfiles/fonts/.fonts/;
 done
