95e712ccbca096235833bb457450745a82f4ae23
Isaac W Hanson <ihanson@sevone.com>
2018-06-21 14:05:48 -0400

    pacaur base

--- home/Documents/custom_iosevka.sh
+++ home/Documents/custom_iosevka.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-yaourt -S --needed --noconfirm otfcc ttfautohint npm
+pacaur -S --needed --noconfirm otfcc ttfautohint npm
 npm install
 for DESIGN in term termlig type; do
   CUSTOM_CONFIG="$DESIGN sans ligset-ml v-l-zshaped v-i-zshaped v-a-singlestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-brace-straight v-dollar-open"
