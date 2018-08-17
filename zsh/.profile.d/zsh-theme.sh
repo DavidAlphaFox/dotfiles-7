4f9821c65ed21c66824cf3170b37001056d263d8
Isaac W Hanson <isaac@starlig.ht>
2018-08-17 17:55:30 -0400

    zsh theme host icon

--- zsh/.profile.d/zsh-theme.sh
+++ zsh/.profile.d/zsh-theme.sh
@@ -507,7 +507,7 @@ function __promptline_host {
   local only_if_ssh="0"
 
   if [ ! $only_if_ssh -o -n "${SSH_CLIENT}" ]; then
-    if [[ -n ${ZSH_VERSION-} ]]; then print " %m"; elif [[ -n ${FISH_VERSION-} ]]; then hostname -s; else printf " %s" \\h; fi
+    if [[ -n ${ZSH_VERSION-} ]]; then print "  %m"; elif [[ -n ${FISH_VERSION-} ]]; then hostname -s; else printf "  %s" \\h; fi
   fi
 }
 
