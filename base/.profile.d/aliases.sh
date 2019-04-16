d26d026f4aa25ce6b5d05e03e5f30961fdbd21d8
Isaac W Hanson <isaac@starlig.ht>
2019-04-16 19:28:04 -0400

    nvim alias

--- base/.profile.d/aliases.sh
+++ base/.profile.d/aliases.sh
@@ -9,5 +9,3 @@ alias gas='git all status -sb'
 alias garp='git all remote prune origin'
 alias drmi="docker images | fzf-tmux -m --tac | awk '{print \$3}' | sort -u | xargs docker rmi -f"
 alias fa='cat ~/Documents/fa_cheat.txt | grep -v Name | grep -i -B1 -e '
-alias ncdu='ncdu --color dark'
-alias vim=nvim
